c57a8810c420ca1d1e3836b2719faa72e6b75680
rmuhammadihsan <rmuhammadihsan01@gmail.com>
2017-04-06 09:43:11 +0700

    Update Jurnal.java

--- Jurnal.java
+++ Jurnal.java
@@ -17,7 +17,7 @@ public class Jurnal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+   
           Scanner sc=new Scanner(System.in);
          int n1=1,n2=1,n3,i; 
         int hasil=n1+n2;
